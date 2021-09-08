73cff2e4a90b3ff614bf5717102a6670abf191d4
Marcelo Eduardo Marques Ribas <contato.marcelomk@hotmail.com>
2021-09-08 17:19:10 -0300

    Layers

---
+++ backend/src/main/java/com/projetovendas/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.projetovendas.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetovendas.dsvendas.dto.SellerDTO;
+import com.projetovendas.dsvendas.entities.Seller;
+import com.projetovendas.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
